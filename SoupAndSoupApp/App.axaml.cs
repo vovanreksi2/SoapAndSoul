12b51d41f055039d6bc7e68e29d3bcd42ea1ef2a
Volodymyr <vovanreksi@gmail.com>
2025-08-03 11:59:19 +0300

    Move extension to separate file

--- SoupAndSoupApp/App.axaml.cs
+++ SoupAndSoupApp/App.axaml.cs
@@ -7,9 +7,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using SoupAndSoup.Data;
 using SoupAndSoupApp.Helpers;
-using SoupAndSoupApp.Services;
 using SoupAndSoupApp.ViewModels;
 using SoupAndSoupApp.Views;
 
@@ -127,34 +125,5 @@ public class App : Application
         var mainWindow = sender as MainWindow;
         mainWindow.Closing -= MainWindowOnClosing; // Unsubscribe from the event to prevent recursion
         mainWindow.Close();
-    }
-}
-
-public static class AppBuilderExtensions
-{
-    public static void UseSoapAndSoulApp(this IServiceCollection services)
-    {
-        services.AddSingleton<MainWindow>();
-        services.AddSingleton<MainViewModel>();
-
-        services.AddSingleton<SoapDesignerView>();
-        services.AddTransient<SoapDesignerViewModel>();
-
-        services.AddSingleton<AddIngredientDialog>();
-        services.AddSingleton<AddIngredientDialogViewModel>();
-
-        services.AddSingleton<IDialogService, DialogService>();
-
-        services.AddSingleton<IDesignerViewModelFactory, DesignerViewModelFactory>();
-        services.AddSingleton<IActiveViewModelRegistry, ActiveViewModelRegistry>();
-
-        services.AddSingleton<INotificationService, NotificationService>();
-
-        services.AddSingleton<IAzureBlobStorageService, AzureBlobStorageService>();
-
-        services.AddSingleton<IUnitCostCalculator, UnitCostCalculator>();
-        services.AddSingleton<MeasureTypeCache>();
-
-        services.AddSoupAndSoulDb();
     }
 }
